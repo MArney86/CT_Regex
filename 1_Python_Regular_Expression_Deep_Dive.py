b1f0f1d6c16c4a042c39116b0003353b3b9a0e3d
MArney86 <matthew.h.arney.1986@gmail.com>
2024-07-04 22:21:37 -0500

    minor change

--- 1_Python_Regular_Expression_Deep_Dive.py
+++ 1_Python_Regular_Expression_Deep_Dive.py
@@ -2,5 +2,5 @@
 import re #given code
 
 text = "Emails: user1@domain.com, user2@exclude.com, user3@domain.com" #given code
-emails = re.findall(r"\b[A-Za-z0-9._%+-]+@(?!exclude.com)[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b", text) #added a ?! to check exclude.com domain isn't there before ensuring that the rest of the domain is part of the valid email
+emails = re.findall(r"\b[A-Za-z0-9._%+-]+@(?!exclude.com)[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b", text) #added a ?! grouping to check exclude.com domain isn't there before ensuring that the rest of the domain is part of the possible valid email
 print(emails) #given code
